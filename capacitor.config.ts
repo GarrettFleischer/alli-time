887d33bc009065d7707a75d30197addebc4d6cb8
Garrett Fleischer <garrett.fleischer@yahoo.com>
2025-03-13 11:45:29 -0700

    Use capacitor for generating iOS project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yourname.allitime',
+  appName: 'alli-time',
+  webDir: 'dist'
+};
+
+export default config;
